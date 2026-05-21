dd42c5e3b852b046e537791d4e6e0cecd6c7b954
vivianmurathimi@gmail.com <your.github.vivianmurathimi@gmail.com>
2026-05-21 15:53:06 +0200

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/beautyhub/beautyhubbackend/BeautyHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beautyhub.beautyhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyHubBackendApplication.class, args);
+    }
+
+}
