ef94d26c9f8401ca0a20922216aaa6d6289f2df7
vivianmurathimi@gmail.com <your.github.vivianmurathimi@gmail.com>
2026-05-22 16:57:29 +0200

    feat: add repository layer - Country, Client, Person, Company

---
+++ src/main/java/com/beautyhub/beautyhubbackend/repository/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.beautyhub.beautyhubbackend.repository;
+
+import com.beautyhub.beautyhubbackend.domain.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Country, Long> {
+
+    // findAll()        ← FREE from JpaRepository
+    // findById(id)     ← FREE from JpaRepository
+    // save(country)    ← FREE from JpaRepository
+    // deleteById(id)   ← FREE from JpaRepository
+    // existsById(id)   ← FREE from JpaRepository
+
+}
